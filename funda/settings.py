33de32d8ad2509a5ed1f3fd06a0be2b1ab6454df
Terence <t.s.n.brouns@gmail.com>
2022-03-19 21:59:33 +0100

    Updated to work with latest funda

--- funda/settings.py
+++ funda/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'funda.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'funda (+http://www.yourdomain.com)'
+USER_AGENT = 'tbrouns (Personal use)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- funda/spiders/funda_spider.py
+++ funda/spiders/funda_spider.py
@@ -8,9 +8,11 @@ class FundaSpider(scrapy.Spider):
 
     name = "funda_spider"
     allowed_domains = ["funda.nl"]
-    start_urls = ["https://www.funda.nl/koop/amsterdam/p%s/" % (page_number) for page_number in range(500)]
-    base_url = "https://www.funda.nl/koop/amsterdam/"
-    le1 = LinkExtractor(allow=r'%s+(huis|appartement)-\d{8}' % base_url)
+
+    def __init__(self, place='amsterdam', range_max=300):
+        self.start_urls = ["https://www.funda.nl/koop/%s/p%s/" % (place, page_number) for page_number in range(1, int(range_max))]
+        self.base_url = "https://www.funda.nl/koop/%s/" % place
+        self.le1 = LinkExtractor(allow=r'%s+(huis|appartement)-\d{8}' % self.base_url)
 
     def parse(self, response):
         links = self.le1.extract_links(response)
@@ -28,21 +30,31 @@ class FundaSpider(scrapy.Spider):
         new_item = response.request.meta['item']
         title = response.xpath('//title/text()').extract()[0]
         postal_code = re.search(r'\d{4} [A-Z]{2}', title).group(0)
-        city = re.search(r'\d{4} [A-Z]{2} \w+',title).group(0).split()[2]
+        city = re.search(r'\d{4} [A-Z]{2} \w+\s\w*',title).group(0).split()[2:]
+        city = " ".join(city)
         address = re.findall(r'te koop: (.*) \d{4}',title)[0]
         price_dd = response.xpath('.//strong[@class="object-header__price"]/text()').extract()[0]
         price = ''.join(re.findall(r'\d+', price_dd)).replace('.','')
         year_built = self.constructionYear(response)
-        area_dd = response.xpath("//dt[text()='Wonen']/following-sibling::dd[1]/span/text()").extract()[0]
-        area = re.findall(r'\d+', area_dd)[0]
+        living_area = self.get_area(response, "Wonen")
+        plot_size = self.get_area(response, "Perceel")
+        backyard_area = self.get_area(response, "Achtertuin")
+        sideyard_area = self.get_area(response, "Zijtuin")
+        yard_area = str(float(backyard_area) + float(sideyard_area))
         bedrooms = response.xpath("//span[contains(@title,'slaapkamer')]/following-sibling::span[1]/text()").extract()[0]
-
+        bathrooms, toilets = self.get_bathrooms(response)
+        energy_label = self.get_entry(response, "Energielabel")
         new_item['postal_code'] = postal_code
         new_item['address'] = address
         new_item['price'] = price
         new_item['year_built'] = year_built
-        new_item['area'] = area
+        new_item['living_area'] = living_area
+        new_item['plot_size'] = plot_size
+        new_item['backyard_area'] = yard_area
+        new_item['energy_label'] = energy_label
         new_item['bedrooms'] = bedrooms
+        new_item['bathrooms'] = bathrooms
+        new_item['toilets'] = toilets
         new_item['city'] = city
         yield new_item
     
@@ -62,3 +74,27 @@ class FundaSpider(scrapy.Spider):
         except:
             return "Failed to parse"
 
+
+    def get_entry(self, response, name):
+        xpath = "//dt[text()='" + name + "']/following-sibling::dd[1]/span/text()"
+        entry = response.xpath(xpath).extract()
+        if len(entry) > 0:
+            entry = entry[0]
+        return entry
+
+    def get_area(self, response, name):
+        area_dd = self.get_entry(response, name)
+        if len(area_dd) > 0:
+            area = re.findall(r'\d+', area_dd)[0]
+        else:
+            area = 0.0
+        return area
+
+    def get_houseType(self, response):
+        pass
+
+    def get_bathrooms(self, response):
+        bathrooms = self.get_entry(response, name="Aantal badkamers")
+        number_of_rooms = re.findall(r'\d+', bathrooms)
+        bathrooms, toilets = number_of_rooms
+        return bathrooms, toilets
