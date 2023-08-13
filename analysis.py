1eb18600b4957970ea47c14f4459c7d493e25189
Terence <t.s.n.brouns@gmail.com>
2023-08-13 15:00:52 +0200

    some analysis, added requirements

--- analysis.py
+++ analysis.py
@@ -3,6 +3,8 @@ import glob
 import numpy as np
 import os
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
 
 json_dir = "./output"
 json_path_list = sorted(glob.glob(os.path.join(json_dir, "*.json")))
@@ -42,7 +44,8 @@ def get_backyard_area(entry):
         living_area = convert_to_float(living_area)
         plot_size = convert_to_float(plot_size)
         if living_area is not None and plot_size is not None:
-            backyard_area = plot_size - living_area
+            # backyard_area = plot_size - living_area / 2
+            backyard_area = entry["plot_size"]
         elif backyard_area == "unknown":
             backyard_area = 0.0
     backyard_area = convert_to_float(backyard_area)
@@ -77,6 +80,14 @@ for key in result_dict.keys():
     rooms = convert_field_to_float(entry, "rooms")
 
     if (
+        # entry["town"] != "Helvoirt"
+        entry["town"] != "Sint-Michielsgestel"
+        # and entry["town"] != "Boxtel"
+        # and entry["town"] != "Haaren"
+    ):
+        continue
+
+    if (
         price is not None
         and year_built is not None
         and living_area is not None
@@ -87,8 +98,31 @@ for key in result_dict.keys():
         features.append([year_built, living_area, backyard_area, energy_label, rooms])
         house_prices.append(price)
 
-y = np.array(house_prices)
-X = np.array(features)
+print("Aantal huizen:")
+print(np.sum((np.array(features)[:, 1] > 120) * (np.array(features)[:, 2] > 300) * (np.array(house_prices) < 700000)))
+print()
 
-reg = LinearRegression().fit(X, y)
-reg.predict(np.array([[2004, 90, 0, 3]]))
+# y = np.array(house_prices)
+# X = np.array(features)
+#
+# reg = LinearRegression().fit(X, y)
+# # year_built, living_area, backyard_area, energy_label,
+# x = np.array([[1984, 140, 368, 4, 4]])  # Helvoirt
+# x0 = np.array([[2012, 160, 320, 4, 3]])  # De Goudplevier 14
+# x1 = np.array([[1975, 177, 320, 4, 4]])  # Sint Jorisstraat 29
+# y_pred = reg.predict(np.concatenate((x0, x1)))
+# print(y_pred.astype(int))
+#
+#
+# # Creating figure
+# fig = plt.figure(figsize=(10, 7))
+# ax = plt.axes(projection="3d")
+#
+# # Creating plot
+# ax.scatter3D(X[:, 0], X[:, 1], y, color="green", alpha=0.5, s=2)
+# ax.scatter3D(x0[:, 0], x0[:, 1], 595000, color="red")
+# ax.scatter3D(x1[:, 0], x1[:, 1], 595000, color="blue")
+# ax.set_xlim([1900, 2030])
+# ax.set_ylim([0, 500])
+# ax.set_zlim([0, 1e6])
+# plt.show()
